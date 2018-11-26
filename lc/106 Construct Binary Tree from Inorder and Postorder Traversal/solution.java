a6c9b1a561dbc37ea20b4385b587b0255b3d2f0a
weidi0629 <weidi.zhang0629@gmail.com>
2018-11-26 15:24:23 -0600

    Update solution.java

--- lc/106 Construct Binary Tree from Inorder and Postorder Traversal/solution.java
+++ lc/106 Construct Binary Tree from Inorder and Postorder Traversal/solution.java
@@ -12,6 +12,6 @@ public TreeNode buildTree(int[] in, int[] post) {
       }
     }
     root.right = helper(post, idx - 1, in, i + 1, end);
-    root.left = helper(post, idx - (end - i  + 1), in, start, i - 1);
+    root.left = helper(post, idx - (end - i  + 1), in, start, i - 1); // 注意i不是一个计数，而是从start开始
     return root;    
   }
