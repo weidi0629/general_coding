d1fdaff2245a6d3215067fbc46ca2a4bee4cd2b4
weidi0629 <weidi.zhang0629@gmail.com>
2018-12-03 17:42:37 -0600

    Update pandas.py

--- pandas.py
+++ pandas.py
@@ -1,3 +1,6 @@
 # group by multiple col, count, and add two df
 res =  tmpc.groupby(['IDNUM','FAdd','FSAdd']).count().add\
             (res.groupby(['IDNUM','FAdd','FSAdd']).count(),fill_value=0).reset_index()
+
+df1 = df.copy() # deep copy is in default, deep copy means create a new object
+df2 = df.copy(deep=false) # deep is false, means it if a reference
