e7d9fa5a2f7c5a05963b9028f27cc6359456a667
weidi0629 <weidi.zhang0629@gmail.com>
2019-04-27 22:24:09 -0500

    Create trie.cpp

---
+++ lc/trie/527. Word Abbreviation/trie.cpp
@@ -0,0 +1,54 @@
+/*
+  1. 没有定义正规的add, search 方程，直接在内部遍历trie了
+  
+*/
+
+
+class Solution {
+    struct Trie {
+        shared_ptr<Trie> child[26];
+        int count{0};
+        }
+    };
+public:
+    vector<string> wordsAbbreviation(vector<string>& dict) {
+        unordered_map<string, vector<int>> group;
+        vector<int> prev(dict.size(), 1); // use single char as prefix by default
+        for (int i = 0; i < dict.size(); ++i) {
+            auto abbr = abbrev(dict[i], prev[i]);
+            group[abbr].push_back(i);
+        }
+        
+        vector<string> result(dict.size());
+        for (auto &p:group) {
+            auto trie = make_shared<Trie>();
+            for (int i:p.second) {
+                auto ptr = trie;
+                for (char c:dict[i]) {
+                    if (ptr->child[c-'a'] == nullptr) ptr->child[c-'a'] = make_shared<Trie>();
+                    ptr = ptr->child[c-'a'];
+                    ++ptr->count;
+                }
+            }
+            for (int i:p.second) {
+                auto ptr = trie;
+                int prefix = 1;
+                for (char c:dict[i]) {
+                    if (ptr->child[c-'a']->count == 1) break;
+                    ptr = ptr->child[c-'a'];
+                    ++prefix;
+                }
+                result[i] = abbrev(dict[i], prefix);
+            }
+        }
+        return result;
+    }
+    
+    
+    string abbrev(string &str, int k) { // k is length of prefix
+        if (str.size() - k <= 2) {
+            return str;
+        }
+        return str.substr(0, k) + to_string(str.size()-k-1) + str.back();
+    }
+};
