9b34adf43f8f016c33a384e7faa6fbcc95f11bb5
weidi0629 <weidi.zhang0629@gmail.com>
2019-05-13 10:17:49 -0500

    Update solution.py

--- lc/python.skills/ZIPzip/14. Longest Common Prefix/solution.py
+++ lc/python.skills/ZIPzip/14. Longest Common Prefix/solution.py
@@ -1,6 +1,6 @@
 if len(strs) == 0:
             return ""
-        for i, group in enumerate(zip(*strs)):
-            if(len(set(group)) >1): #group是横过来的每一个字符
+        for i, group in enumerate(zip(*strs)): # zip的输出是tuple ('w','w','g')
+            if(len(set(group)) >1): #group是横过来的每一个字符, set 能去掉重复
                 return strs[0][:i]
         return min(strs)
