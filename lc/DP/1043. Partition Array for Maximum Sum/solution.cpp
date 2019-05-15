956f2b729c871678e5d52892f144f87ae7eddf1a
weidi0629 <weidi.zhang0629@gmail.com>
2019-05-14 23:14:22 -0500

    Create solution.cpp

---
+++ lc/DP/1043. Partition Array for Maximum Sum/solution.cpp
@@ -0,0 +1,16 @@
+/*
+有点像 cutting rod, 取后面k个里面最大的值。因为题设是要返回和，所以公式里面是加，但想法还是一样的。
+*/
+
+int maxSumAfterPartitioning(vector<int>& A, int K) {
+        int N = A.size();
+        vector<int> dp(N);
+        for (int i = 0; i < N; ++i) {
+            int curMax = 0;
+            for (int k = 1; k <= K && i - k + 1 >= 0; ++k) {
+                curMax = max(curMax, A[i - k + 1]);
+                dp[i] = max(dp[i], (i >= k ? dp[i - k] : 0) + curMax * k);
+            }
+        }
+        return dp[N - 1];
+    }
