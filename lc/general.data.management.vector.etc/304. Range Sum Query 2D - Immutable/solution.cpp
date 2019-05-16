734abaabe2e8eb9424d404c4f49e1fbcf76f33ef
weidi0629 <weidi.zhang0629@gmail.com>
2019-05-16 11:51:23 -0500

    Create solution.cpp

---
+++ lc/general.data.management.vector.etc/304. Range Sum Query 2D - Immutable/solution.cpp
@@ -0,0 +1,26 @@
+/*
+关键是这个sum[i][j]矩阵记录着矩阵 i-1, j-1的面积。从小到大慢慢记录，这点非常重要。
+https://leetcode.com/problems/range-sum-query-2d-immutable/discuss/75350/Clean-C%2B%2B-Solution-and-Explaination-O(mn)-space-with-O(1)-time
+*/
+
+class NumMatrix {
+private:
+    int row, col;
+    vector<vector<int>> sums;
+public:
+    NumMatrix(vector<vector<int>> &matrix) {
+        row = matrix.size();
+        col = row>0 ? matrix[0].size() : 0;
+        sums = vector<vector<int>>(row+1, vector<int>(col+1, 0));
+        for(int i=1; i<=row; i++) {
+            for(int j=1; j<=col; j++) {
+                sums[i][j] = matrix[i-1][j-1] + 
+                             sums[i-1][j] + sums[i][j-1] - sums[i-1][j-1] ;
+            }
+        }
+    }
+
+    int sumRegion(int row1, int col1, int row2, int col2) {
+        return sums[row2+1][col2+1] - sums[row2+1][col1] - sums[row1][col2+1] + sums[row1][col1];
+    }
+};
