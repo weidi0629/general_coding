5289a63c7e0f5e3fe40b3202010da9b772f2d257
weidi0629 <weidi.zhang0629@gmail.com>
2019-05-27 11:32:19 -0500

    Create 1st.regular.solution.cpp

---
+++ lc/DP/Knapsacks/1049. Last Stone Weight II/1st.regular.solution.cpp
@@ -0,0 +1,26 @@
+/*
+这题关键是转换问题成 knapsacks, 
+
+
+*/
+
+class Solution {
+public:
+    int lastStoneWeightII(vector<int>& stones) {
+        int S=0, s2 = 0;
+        int n = stones.size();
+        for(auto s:stones) S +=s;
+        vector<vector<bool>> dp = {S+1,vector<bool>(n+1,false)};
+        for(int i=0; i<n; i++){
+            dp[0][i] = true;
+        }
+        for(int s=1; s<=S/2; s++) // knapsack, 最大的weight是总重量的一半 
+            for(int i=1; i<=n; i++){
+                if(dp[s][i-1]||i-stones[i-1]>0&&dp[i-stones[i-1]][i-1]){
+                    dp[s][i] = true; // 这里的i处理的是 i-1之前的事情
+                    s2 = max(s2,s);
+                }
+            }
+        return S - 2*s2;
+    }
+};
