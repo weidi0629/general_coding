c0d87ee1e4dbcfcb0a4b41a0e2718e9016eaf046
weidi0629 <weidi.zhang0629@gmail.com>
2019-06-01 21:35:08 -0500

    Update solution.cpp

--- lc/DP/1043. Partition Array for Maximum Sum/solution.cpp
+++ lc/DP/1043. Partition Array for Maximum Sum/solution.cpp
@@ -1,5 +1,10 @@
 /*
 有点像 cutting rod, 取后面k个里面最大的值。因为题设是要返回和，所以公式里面是加，但想法还是一样的。
+
+dp[i] 表示到i位置所能求的最大的值 （也是题设的要求）
+
+那比如说，最大值所能达到的状态是  [ .... j iiiii] (iiii表示后面几个数都是一样)， 而dp的subproblem就保证了 dp[j]是最优的。寻找的范围就是K
+
 */
 
 int maxSumAfterPartitioning(vector<int>& A, int K) {
