daaaa7d49ba91eecfd89f87155d68fdacb356413
weidi0629 <weidi.zhang0629@gmail.com>
2019-06-04 21:40:31 -0500

    Create solution.cpp

---
+++ lc/search.binary.search.etc/719. Find K-th Smallest Pair Distance - trial and error/solution.cpp
@@ -0,0 +1,27 @@
+public int smallestDistancePair(int[] nums, int k) {
+    Arrays.sort(nums);
+    
+    int n = nums.length;
+    int l = 0;
+    int r = nums[n - 1] - nums[0];
+    
+    for (int cnt = 0; l < r; cnt = 0) {
+        int m = l + (r - l) / 2; // 假设这是成立的
+        
+        for (int i = 0, j = 0; i < n; i++) {
+            while (j < n && nums[j] <= nums[i] + m) j++; // 这种题目的关键，就是如何找这个与你找到中点的值比较的条件，这个条件必须简单。这里因为是排序过得，
+            有后面一个可以代表前面一个的概念在
+            cnt += j - i - 1;
+        }
+        
+        if (cnt < k) {
+            l = m + 1;
+        } else {
+            r = m;
+        }
+    }
+    
+    return l;
+}
+
+
