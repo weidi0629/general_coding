d0d0ccdc3125350bc9fdc50c805cb6831f26e2f5
weidi0629 <weidi.zhang0629@gmail.com>
2019-06-18 10:52:51 -0500

    Update using.bitset.cpp

--- lc/DP/Knapsacks/1049. Last Stone Weight II/using.bitset.cpp
+++ lc/DP/Knapsacks/1049. Last Stone Weight II/using.bitset.cpp
@@ -4,12 +4,12 @@ dp[x] = 1 means the sum x is possible.
 */
 
 int lastStoneWeightII(vector<int> A) {
-        bitset<1501> dp = {1};
+        bitset<1501> dp = {1};  // 0是肯定可以的
         int sumA = 0, res = 100;
         for (int a : A) {
             sumA += a;
             for (int i = sumA; i >= a; --i)
-                dp[i] = dp[i] + dp[i - a]; // 这个相加并不会进位，跟或是一样的意思
+                dp[i] = dp[i] + dp[i - a]; // 这个相加并不会进位，跟或是一样的意思. dp[i]要么本身进行，或者[i-a]可以，那加上a也可以
         }
         for (int i = sumA / 2; i > 0; --i)
             if (dp[i]) return sumA - i - i;
