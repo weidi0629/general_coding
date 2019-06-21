b6bfecfe8a7cc754510428fd74543738ef3e65ff
weidi0629 <weidi.zhang0629@gmail.com>
2019-06-21 14:38:59 -0500

    Create normal.cpp

---
+++ lc/unionFind/721. Accounts Merge/normal.cpp
@@ -0,0 +1,38 @@
+/*
+两拨emails，只要里面有一个相同的，就会讲两拨以parent的数组练习起来
+*/
+
+class Solution {
+public:
+    vector<vector<string>> accountsMerge(vector<vector<string>>& acts) {
+        map<string, string> owner;
+        map<string, string> parents;
+        map<string, set<string>> unions;
+        for (int i = 0; i < acts.size(); i++) {
+            for (int j = 1; j < acts[i].size(); j++) {
+                parents[acts[i][j]] = acts[i][j];
+                owner[acts[i][j]] = acts[i][0];
+            }
+        }
+        for (int i = 0; i < acts.size(); i++) {
+            string p = find(acts[i][1], parents); // 每一波都用第一个作为parent
+            for (int j = 2; j < acts[i].size(); j++)
+                parents[find(acts[i][j], parents)] = p; // 只要有一个相同，前一波的头的parent就是这波的头，两拨就连起来了
+        }
+        for (int i = 0; i < acts.size(); i++)
+            for (int j = 1; j < acts[i].size(); j++)
+                unions[find(acts[i][j], parents)].insert(acts[i][j]);  //从上面的注释可以看到，无论多少波，头都被赋值成一样的
+
+        vector<vector<string>> res;
+        for (pair<string, set<string>> p : unions) {
+            vector<string> emails(p.second.begin(), p.second.end());
+            emails.insert(emails.begin(), owner[p.first]);
+            res.push_back(emails);
+        }
+        return res;
+    }
+private:
+    string find(string s, map<string, string>& p) {
+        return p[s] == s ? s : find(p[s], p);
+    }
+};
