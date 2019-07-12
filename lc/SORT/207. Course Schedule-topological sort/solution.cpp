312bcfec7df3afaec8c95067099d91a2c22c8c5f
weidi0629 <weidi.zhang0629@gmail.com>
2019-07-11 22:37:22 -0500

    Create solution.cpp

---
+++ lc/SORT/207. Course Schedule-topological sort/solution.cpp
@@ -0,0 +1,74 @@
+/*
+这题给出了两个topologic sort的解法，一个是degree, 一个是dfs 本轮-总共的
+dfs的方法: 如果是遇到本轮的经过过的点，就说明有cycle了。如果是遇到之前的点，应该没问题。
+
+-->1 --->2
+    -->3
+对于三来说，上一轮是2这个箭头，1这一断不算的，因为还算在3这一轮里面  
+
+
+*/
+
+// degree solution
+bool canFinish(int n, vector<pair<int, int>>& pre) {
+    vector<vector<int>> adj(n, vector<int>());
+    vector<int> degree(n, 0);
+    for (auto &p: pre) {
+        adj[p.second].push_back(p.first);
+        degree[p.first]++;
+    }
+    queue<int> q;
+    for (int i = 0; i < n; i++)
+        if (degree[i] == 0) q.push(i);
+    while (!q.empty()) {
+        int curr = q.front(); q.pop(); n--;
+        for (auto next: adj[curr])
+            if (--degree[next] == 0) q.push(next);
+    }
+    return n == 0;
+}
+
+
+// dfs
+class Solution {
+public:
+    bool canFinish(int numCourses, vector<pair<int, int>>& prerequisites) {
+        graph g = buildGraph(numCourses, prerequisites);
+        vector<bool> todo(numCourses, false), done(numCourses, false);
+        for (int i = 0; i < numCourses; i++) {
+            if (!done[i] && !acyclic(g, todo, done, i)) {
+                return false;
+            }
+        }
+        return true;
+    }
+private:
+    typedef vector<vector<int>> graph;
+    
+    graph buildGraph(int numCourses, vector<pair<int, int>>& prerequisites) {
+        graph g(numCourses);
+        for (auto p : prerequisites) {
+            g[p.second].push_back(p.first);
+        }
+        return g;
+    }
+    
+    bool acyclic(graph& g, vector<bool>& todo, vector<bool>& done, int node) {
+        if (todo[node]) {  // 如果是本轮，就直接回绝掉
+            return false;
+        }
+        if (done[node]) { // 如果是上一轮的，没问题      
+            return true;
+        }
+        todo[node] = done[node] = true;
+        for (int v : g[node]) {
+            if (!acyclic(g, todo, done, v)) {
+                return false;
+            }
+        }
+        todo[node] = false; // 结束不要忘记本轮清空 
+        return true;
+    }
+};
+
+
