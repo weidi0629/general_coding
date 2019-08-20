e3899890b72637f53d9d602b59ccdf4b1486c10e
weidi0629 <weidi.zhang0629@gmail.com>
2019-08-19 23:39:54 -0500

    Update solution.cpp

--- lc/DP/1092. Shortest Common Supersequence - how to convert to lcs/solution.cpp
+++ lc/DP/1092. Shortest Common Supersequence - how to convert to lcs/solution.cpp
@@ -22,7 +22,9 @@ string shortestCommonSupersequence(string& A, string& B) {
         for (int i = 0; i < n; ++i)
             for (int j = 0; j < m; ++j)
                 if (A[i] == B[j])
-                    dp[i + 1][j + 1] = dp[i][j] + A[i];
+                    dp[i + 1][j + 1] = dp[i][j] + A[i]; // dp[i+1][j], dp[i][j+1]不可能有dp[i][j]+1大. 因为dp[i+1][j], dp[i][j+1]最后一
+            // 个不一样的话，无论LCS 是什么，肯定再加上1更大。如果一样的话，还是加上1更大。
+            //
                 else
                     dp[i + 1][j + 1] = dp[i + 1][j].size() > dp[i][j + 1].size() ?  dp[i + 1][j] : dp[i][j + 1];
         return dp[n][m];
