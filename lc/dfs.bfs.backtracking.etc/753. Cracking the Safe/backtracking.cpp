69e9edb2cce423c12cd4eb744e45d123a916afec
weidi0629 <weidi.zhang0629@gmail.com>
2019-10-31 22:23:55 -0500

    Create backtracking.cpp

---
+++ lc/dfs.bfs.backtracking.etc/753. Cracking the Safe/backtracking.cpp
@@ -0,0 +1,36 @@
+/*
+有几种？ -> 如何能结束dfs
+真的要很长的string吗，有没有其他的办法
+*/
+
+class Solution {
+public:
+    string crackSafe(int n, int k) {
+        int total = pow(k,n); //总共的亮
+        string s{n,0};
+        unordered_set<string> visited;
+        visited.insert(s);
+        dfs(s,visited,n,k,total);
+        return s;
+    }
+    
+    bool dfs(string& s, unordered_set<string> visited,int n, int k,int total){
+        if(visited.size()==total) return true;
+        string prev = s.substr(s.size()-n+1); // 比n少一位      
+        for(int i=0;i<k;i++){ //begin the backtracking
+            if(visited.find(prev+to_string(i))!=visited.end()){ //之前没有过
+                visited.insert(prev+to_string(i));
+                s = s+to_string(i);
+                if(dfs(s,visited,n,k,total)) return true;
+                //没有找到，归位，因为这个数在别人的round里也可能需要
+                //注意所有的可能性都要查，所以参数没有向前，回到最原始的位数
+                else
+                {
+                    s = s.substr(0,s.size()-1);
+                    visited.erase(prev+to_string(i));
+                }
+            }
+        }
+        return false; // 这句漏掉了
+    }
+};
