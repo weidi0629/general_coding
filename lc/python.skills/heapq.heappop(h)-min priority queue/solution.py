0c505d177660540d6f3a0933c3dad4ce73122c25
weidi0629 <weidi.zhang0629@gmail.com>
2020-01-09 10:38:14 -0600

    Update solution.py

--- lc/python.skills/heapq.heappop(h)-min priority queue/solution.py
+++ lc/python.skills/heapq.heappop(h)-min priority queue/solution.py
@@ -14,3 +14,24 @@
             for j in xrange(len(bikes)):
                 if taken & (1 << j) == 0:
                     heapq.heappush(h, [cost + dis(i, j), i + 1, taken | (1 << j)])  # 用法 
+
+                
+# 632 Smallest Range Covering Elements from K Lists
+'''
+这题的算法思路跟cpp一样，只判断每个list第一位，以及所有遍历一来的最大值。主要这里要学习的还是headpq的用法。
+'''
+def smallestRange(self, A):
+    pq = [(row[0], i, 0) for i, row in enumerate(A)]
+    heapq.heapify(pq)
+    
+    ans = -1e9, 1e9
+    right = max(row[0] for row in A)
+    while pq:
+        left, i, j = heapq.heappop(pq)
+        if right - left < ans[1] - ans[0]:
+            ans = left, right
+        if j + 1 == len(A[i]):
+            return ans
+        v = A[i][j+1]
+        right = max(right, v)
+        heapq.heappush(pq, (v, i, j+1))
