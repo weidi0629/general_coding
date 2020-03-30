597c7fb926222cfd34d880dd5769564b72a42cc3
weidi0629 <weidi.zhang0629@gmail.com>
2020-03-30 13:36:42 -0500

    Create define-2d-defaultdict.py

---
+++ lc/python.skills/defaultdict/define-2d-defaultdict.py
@@ -0,0 +1,22 @@
+#1396. Design Underground System
+
+class UndergroundSystem:
+    def __init__(self):
+        self.times = defaultdict(lambda : defaultdict(lambda : [0, 0]))
+        self.transit = defaultdict(list)
+        
+
+    def checkIn(self, id: int, sname: str, t: int) -> None:
+        self.transit[id].extend([sname, t])
+
+    def checkOut(self, id: int, stationName: str, t: int) -> None:
+        srcname, srct = self.transit[id]
+        count, val = self.times[srcname][stationName] 
+        self.times[srcname][stationName] = [count+1, val+t-srct] 
+        self.transit[id] = []
+
+    def getAverageTime(self, startStation: str, endStation: str) -> float:
+        count, val = self.times[startStation][endStation] 
+        return val / count
+        
+  # https://stackoverflow.com/questions/8419401/python-defaultdict-and-lambda
