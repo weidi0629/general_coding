2f181ce8285aec68e1d932724c74b8af920485fc
weidi0629 <weidi.zhang0629@gmail.com>
2020-06-05 12:16:19 -0500

    Update quick_search.py

--- lc/python.skills/quick_search.py
+++ lc/python.skills/quick_search.py
@@ -7,3 +7,6 @@ return bisect.bisect_left(self.w, a)
 # get random number, range a to b
 random.randint(a,b) 
 
+# 1d, 2d dictionary 
+self.times = defaultdict(lambda : defaultdict(lambda : [0, 0]))
+self.transit = defaultdict(list)
