e7e27efa7ad855480bbb80752375ea0e1bcba653
weidi0629 <weidi.zhang0629@gmail.com>
2020-06-14 12:01:22 -0500

    Update quick_search.py

--- lc/python.skills/quick_search.py
+++ lc/python.skills/quick_search.py
@@ -10,3 +10,16 @@ random.randint(a,b)
 # 1d, 2d dictionary 
 self.times = defaultdict(lambda : defaultdict(lambda : [0, 0]))
 self.transit = defaultdict(list)
+
+# heap/priority queue with tuples 
+heap = [(a,b,c)]
+aa,bb,cc=heapq.heappop(heap)
+heapq.heappush(heap,(i,j,k))
+
+# 2-d dic
+d = collections.defaultdict(dict)
+for a, b, p in arr:
+    d=[a][b] = p
+# loop the 1st level, it would output the 2nd level key, NOT a dictionary
+for i in d[x]:
+  print(i) # 2nd level key
