302bc1df0f5231b94e67bd57dfa97a4dfd51bb46
weidi0629 <weidi.zhang0629@gmail.com>
2020-07-11 14:33:56 -0500

    Create bit_manipulation.py

---
+++ bit_manipulation.py
@@ -0,0 +1,13 @@
+a>>b 
+# read from left to right
+# a move b position to right
+
+2: 010, the 2nd position appear every 2 number: 2-010-yes, 3-011-yes,4-100-no,5-101-no,6-110-yes,7-111-yes,8-1000-no...
+4: 100, the 3rd poistion appear every 4 number: 4-01000-yes, 5-0101-yes,6-0110-yes,7-0111-yes,8-1000-no...  
+...
+
+
+
+
+
+
