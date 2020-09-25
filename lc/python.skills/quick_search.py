ade4aa21b8a9476c617a9b9130c661937308f3c6
weidi0629 <weidi.zhang0629@gmail.com>
2020-09-25 11:52:25 -0500

    Update quick_search.py

--- lc/python.skills/quick_search.py
+++ lc/python.skills/quick_search.py
@@ -36,3 +36,12 @@ pos1 = bisect_left(SList, nums[i] - t)
 # SList.remove(2) 
 # SList=[1,2,3]
 #https://leetcode.com/problems/contains-duplicate-iii/discuss/824603/Python-SortedList-O(n-log-k)-solution-explained.
+
+
+# Python different solutions (bubble, insertion, selection, merge, quick sorts) and functools.cmp_to_key
+https://leetcode.com/problems/largest-number/discuss/53298/Python-different-solutions-(bubble-insertion-selection-merge-quick-sorts)
+    
+functools.cmp_to_key(func)
+Transform an old-style comparison function to a key function. Used with tools that accept key functions (such as sorted(), min(), max(), heapq.nlargest(), heapq.nsmallest(), itertools.groupby()). This function is primarily used as a transition tool for programs being converted from Python 2 which supported the use of comparison functions.
+
+A comparison function is any callable that accept two arguments, compares them, and returns a negative number for less-than, zero for equality, or a positive number for greater-than. A key function is a callable that accepts one argument and returns another value to be used as the sort key.
